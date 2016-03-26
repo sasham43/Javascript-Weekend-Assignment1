164c038af75495ff3a7f146da078061fb9ee0413
sasham43 <sashasemail@gmail.com>
2016-03-26 09:15:17 -0500

    Add percentCovered function

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ var claim5 = {
 	visitCost: 770
 }
 
+// generate new claims
 var claim6 = new claim('Bob Jones', 'Specialist', 440);
 var claim7 = new claim('Scarf Grommet', 'Emergency', 2000);
 var claim8 = new claim('Docent Brimsby', 'Optical', 300);
@@ -36,18 +37,47 @@ var claim10 = new claim('Eleanor Burnston', 'Primary Care', 2000);
 
 var initialList = [claim1, claim2, claim3, claim4, claim5];
 
+// add new claims to array
 initialList.push(claim6, claim7, claim8, claim9, claim10);
 
-console.log(initialList);
+console.log(percentCovered(claim1));
 
 var totalPayedOut = 0;
+
+//function to determine percent covered
+function percentCovered(claim){
+	// pull variable values out of object for processing
+	var name = claim.name;
+	var visitType = claim.visitType;
+	var cost = claim.visitCost;
+
+	// initialize percentCovered, to be generated based on visit type and then returned
+	var percentCovered = 0.0;
+
+	switch(visitType){
+		case "Optical":
+			percentCovered = 0.0;
+			break;
+		case "Specialist":
+			percentCovered = 0.1;
+			break;
+		case "Primary Care":
+			percentCovered = 0.5;
+			break;
+		case "Emergency":
+			percentCovered = 1.0;
+			break;
+		default:
+			alert("Visit Type not recognized");
+		}
+
+		return percentCovered;
+}
+
+//function to determine amount covered
 
 function claim(name, type, cost){
 	this.patientName = name;
 	this.visitType = type;
 	this.visitCost = cost;
 }
-
-//function to determine percent covered
-
-//function to determine amount covered
